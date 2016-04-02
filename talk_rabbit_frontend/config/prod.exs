c037bef283e6051cada3767cf0044140dff8e736
Dylan Griffith <dyl.griffith@gmail.com>
2016-04-02 22:01:21 +1100

    frontend: More PWS stuff (websockets still not working)

--- talk_rabbit_frontend/config/prod.exs
+++ talk_rabbit_frontend/config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :talk_rabbit_frontend, TalkRabbitFrontend.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: "dylans-demo.cfapps.io", port: 80],
   cache_static_manifest: "priv/static/manifest.json",
   server: true
 
